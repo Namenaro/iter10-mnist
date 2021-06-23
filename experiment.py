594aef1d6c9107116b5366f8c1550e131e8a6b7c
neuro <nanenaro@gmail.com>
2021-06-23 18:29:35 +0300

    содан набросок эксперимента, код пока не рабочий

--- experiment.py
+++ experiment.py
@@ -2,3 +2,13 @@
 # А именно, нужно показать, что чем больше опорных точек Б, тем светлее истинная точка А.
 # Радиусы назначим волюнтаристски. Частоту встречания бешек и ашки пока учитывать не будем,
 # речь идет только про невязку.
+
+def init_A_and_all_hypos():
+    return A, hypotheses
+
+def visualise_A_and_hypos(A, hypotheses):
+    pass
+
+if __name__ == "__main__":
+    A, hypotheses = init_A_and_all_hypos()
+    visualise_A_and_hypos(A, hypotheses)
